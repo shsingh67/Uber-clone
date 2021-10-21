e8c76a36b7e3042e8a07350d7509b84ba696c12d
shshingh67 <singhsharandeep676@gmail.com>
2021-10-21 09:25:07 -0700

    initial commit

---
+++ slices/navSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  origin: null,
+  destination: null,
+  travelTimeInformation: null,
+};
+
+export const navSlice = createSlice({
+  name: "nav",
+  initialState,
+  reducer: {
+    setOrigin: (state, action) => {
+      state.origin = action.payload.origin;
+    },
+    setDestination: (state, action) => {
+      state.destination = action.payload.origin;
+    },
+    setTravelTimeInformation: (state, action) => {
+      state.travelTimeInformation = action.payload.travelTimeInformation;
+    },
+  },
+});
+
+export const { setOrigin, setDestination, setTravelTimeInformation } =
+  navSlice.actions;
+
+export const selectOrigin = (state) => {
+  state.nav.origin;
+};
+
+export const selectDestination = (state) => {
+  state.nav.destination;
+};
+
+export const selectTravelTimeInformation = (state) => {
+  state.nav.travelTimeInformation;
+};
+
+export default navSlice.reducer;
